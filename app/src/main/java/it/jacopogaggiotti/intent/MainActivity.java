db1accaf852468a966270492b0370b8a4e78b0b7
Jacopo <jacopogaggiotti@protonmail.com>
2023-04-22 18:47:07 +0200

    Sviluppato codice per testare le chiamate con gli intent
    Utilizzato il metodo della classe intent.resolveActivity(getPackageManager) per verificare che ci sia una attività corrsipondente all'intent.
    
    il package manager viene utilizzato per verificare se c'è un'attività che possa gestire l'intent. L'attività è un componente dell'applicazione Android che determina cosa deve essere visualizzato e gestito all'interno dell'applicazione.
    
    La funzione resolveActivity() utilizza il package manager per cercare l'attività corrispondente all'intent. Se l'attività viene trovata, il codice continua con l'esecuzione di startActivity(), che avvia l'attività corrispondente attraverso il package manager.

--- app/src/main/java/it/jacopogaggiotti/intent/MainActivity.java
+++ app/src/main/java/it/jacopogaggiotti/intent/MainActivity.java
@@ -2,7 +2,10 @@ package it.jacopogaggiotti.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View btn1 = findViewById(R.id.button1);
+        View btn2 = findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:3534438929"));
+
+                if(intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
